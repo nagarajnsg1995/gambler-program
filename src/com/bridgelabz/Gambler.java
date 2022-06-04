619139329397d6d12a6aa2ef79fb2abfdbbf5c4f
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-04 19:59:04 +0530

    Added:logic with initial dollers

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 public class Gambler {
+    static int Every_Day_Stack=100;
+    final static int Bet_Every_Game=1;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Gambler Simulator");
