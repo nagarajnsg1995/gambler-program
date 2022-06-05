981642a144e93498697cafefe786efa86507e4c9
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-05 21:38:39 +0530

    Added:logic calculating amount for 20 days

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -7,30 +7,57 @@ public class Gambler {
     static int Every_Day_Stack = 100;
     static int Loss = 0;
     static int gain = 0;
+    static int WinDays = 0;
+    static int LossDays = 0;
+    static int lossing = 0;
+    static int Winning = 0;
 
     public static void main(String[] args) {
         int Stack_Upper = 150;
         int Stack_Lower = 50;
+
+
         System.out.println("Welcome To Gambler Simulator");
-        while (Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
-            double random = Math.floor(Math.random() * 10) % 2;
-            System.out.println(random);
-            if (random == 1) {
-                int win = 1;
-                Every_Day_Stack = Every_Day_Stack + win;
-                gain++;
+        for (int day = 1; day <= 20; day++) {
+            Every_Day_Stack = 100;
+            while (Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
+                double random = Math.floor(Math.random() * 10) % 2;
 
-                System.out.println("wins: " + win + " gains: " + gain);
-                System.out.println("total winning : " + Every_Day_Stack);
+                if (random == 1) {
+                    int win = 1;
+                    Every_Day_Stack = Every_Day_Stack + win;
+                    gain++;
+
+                } else {
+                    int loss = 1;
+                    Every_Day_Stack = Every_Day_Stack - loss;
+                    gain--;
+                }
+            }
+
+            if (Every_Day_Stack == Stack_Upper) {
+                WinDays += 1;
+                Winning = 50;
+
+                System.out.println("winnig amt per day:" + Winning);
+                System.out.println("total amount remaining   : " + Every_Day_Stack);
+
+
             } else {
-                int loss = 1;
-                Every_Day_Stack = Every_Day_Stack - loss;
-                gain--;
+                LossDays += 1;
+                lossing = 50;
+                System.out.println("lossing amt per day:" + lossing);
 
-                System.out.println("loss : " + loss + "gains: " + gain);
-                System.out.println("total lossed : " + Every_Day_Stack);
+                System.out.println("total amount remaining : " + Every_Day_Stack);
+
 
             }
+
+            System.out.println(day);
+
+
         }
+
     }
 }
+
