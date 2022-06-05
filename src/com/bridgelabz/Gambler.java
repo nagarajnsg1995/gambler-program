82b7a916408bcaeb8c8c775cec1d60cd45d9d8b8
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-05 21:26:30 +0530

    Added:dollars for playing game

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -1,10 +1,33 @@
 package com.bridgelabz;
 
+import java.util.*;
+
 public class Gambler {
-    static int Every_Day_Stack=100;
-    final static int Bet_Every_Game=1;
+    final static int Bet_Every_Game = 1;
+    static int Every_Day_Stack = 100;
+    static int Loss = 0;
+    static int gain = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Gambler Simulator");
+
+        double random = Math.floor(Math.random() * 10) % 2;
+        System.out.println(random);
+        if (random == 1) {
+            int win = 1;
+            Every_Day_Stack = Every_Day_Stack + win;
+            gain++;
+
+            System.out.println("wins: " + win + " gains: " + gain);
+            System.out.println("total winning : " + Every_Day_Stack);
+        } else {
+            int loss = 1;
+            Every_Day_Stack = Every_Day_Stack - loss;
+            gain--;
+
+            System.out.println("loss : " + loss + "gains: " + gain);
+            System.out.println("total lossed : " + Every_Day_Stack);
+
+        }
     }
 }
